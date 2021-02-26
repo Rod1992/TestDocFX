97c7eb0a732bad4b5656e92d9623786e4b2229c9
Rodrigo <rodrigomartin96@yahoo.es>
2021-02-26 11:56:38 +0100

    Create Project

---
+++ New Unity Project/Assets/ExampleScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    bool Boolean
+    {
+        get;
+        set;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Myfunction(bool yes, bool ofc)
+    {
+
+    }
+}
